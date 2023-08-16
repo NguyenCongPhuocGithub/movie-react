8b0366944f832ed786714c1776eff0cd17f2938a
huygamcha <lehuynhhuy2002@gmail.com>
2023-08-16 11:17:52 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1>ok</h1>
+        <h2>ok</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
