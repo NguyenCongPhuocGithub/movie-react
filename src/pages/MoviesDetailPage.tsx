f280d9562b6b941ed72ca5775d16881fd7dd3fc1
Cong Phuoc <nguyencongphuoccv@gmail.com>
2023-08-21 21:43:46 +0700

    reload -responsive

--- src/pages/MoviesDetailPage.tsx
+++ src/pages/MoviesDetailPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+import useGetMovies from '../hooks/useGetMovies';
 
 interface Data {
   id: number;
@@ -14,19 +15,25 @@ interface Data {
   known_for_department: string;
 }
 
-const MoviesDetailPage = () => {
+interface MoviesDetailPageType {
+  url?: string;
+}
+
+const MoviesDetailPage = ({ url }: MoviesDetailPageType) => {
   const params = useParams();
   console.log(params);
   const id = params.id ? parseInt(params.id) : 0;
 
-  const [data, setData] = useState<Data[]>([]);
-  const [error, setError] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+  const [dataCredit, setData] = useState<Data[]>([]);
+  const [errorCredit, setErrorCredit] = useState<string>("");
+  const [loadingCredit, setLoadingCredit] = useState<boolean>(false);
+
+  // const { data, loading, error } = useGetMovies(url);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true);
+        setLoadingCredit(true);
         const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`, {
           headers: {
             Authorization:
@@ -38,19 +45,26 @@ const MoviesDetailPage = () => {
       } catch (err) {
         console.log(err);
       } finally {
-        setLoading(false);
+        setLoadingCredit(false);
       }
     };
     fetchData();
   }, [id]);
-
-  console.log('data',data);
-
+  
+  console.log('dataCredit', dataCredit);
   return (
     <div className="container">
       <h1>Movies Detail + Biến ID: {params.id}</h1>
-      {data.map((item) => (
-        <p key={item.id}>{item.name}</p>
+      {dataCredit.map((item) => (
+        <>
+          <div key={item.id}>
+            <p >{item.name}</p>
+            <div>
+              <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${item.profile_path}`} alt={item.name} />
+            </div>
+            <p>{`id credit ${item.id}`}</p>
+          </div>
+        </>
       ))}
     </div>
   )
